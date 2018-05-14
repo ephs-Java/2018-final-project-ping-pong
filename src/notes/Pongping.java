d95c46fa08fd91ed5910e7dadd0f725aeda3864c
Jake Derouin <jpderouin310@gmail.com>
2018-05-14 08:48:48 -0500

    ffjgh,fj,gfjly

--- src/notes/Pongping.java
+++ src/notes/Pongping.java
@@ -4,7 +4,19 @@ public class Pongping {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("SUp losers");     ccccc
+		System.out.println("SUp losers");     
+		
+
+Project: Make a ping pong game out of LED's.  
+The game will simulate a ping pong game with the LED bouncing back and fourth.  The led will move faster as the game progresses and a player gets a point when the opposing player fails to press the button (paddle) in time.  
+
+Need: Wires
+Rasperry Pi
+Monitor 
+HDMI 
+Box for LED's to sit in
+Keyboard
+Software for rasp pi that can output to LED's
 	}
 
 }
