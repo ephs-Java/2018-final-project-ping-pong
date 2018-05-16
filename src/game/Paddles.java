0d65fdf3c315f1e17c0bb8f40b8866e39afb8728
Sophie <90304659@ep-student.org>
2018-05-16 08:48:45 -0500

    pad

---
+++ src/game/Paddles.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Paddles {
+
+	private int paddleposx;
+	private int paddleposy;
+	private final int startxpos;
+	private final int startypos;
+	private final int width;
+	private final int height;
+}
