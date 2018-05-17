10887048e9a19d6bd4e86d1c5a63903b317d741f
Sophie <90304659@ep-student.org>
2018-05-17 08:17:06 -0500

    jbklb

--- src/game/Paddle1.java
+++ src/game/Paddle1.java
@@ -4,16 +4,14 @@ public class Paddle1 {
 
 	private int paddleposx;
 	private int paddleposy;
-	private final int startxpos;
-	private final int startypos;
+	private final int start;
 	private final int width;
 	private final int height;
 	
 	public Paddle1(){
 		this.paddleposx = 
 		this.paddleposy = 
-		this.startxpos = 
-		this.startypos =
+		this.start = 
 		this.width = 2;
 		this.height = 7;
 	}
--- src/game/Paddle2.java
+++ src/game/Paddle2.java
@@ -4,16 +4,14 @@ public class Paddle2 {
 
 	private int paddleposx;
 	private int paddleposy;
-	private final int startxpos;
-	private final int startypos;
+	private final int start;
 	private final int width;
 	private final int height;
 	
 			public Paddle2(){
 				this.paddleposx = 
 				this.paddleposy = 
-				this.startxpos = 
-				this.startypos =
+				this.start = 
 				this.width = 2;
 				this.height = 7;
 	}
